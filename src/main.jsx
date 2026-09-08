d46e39ff2902438e0ead5b87626bcff83c28b38b
nielsfrsn <niels.ferson@hotmail.com>
2026-09-08 09:08:42 +0200

    Load mobile header alignment overrides

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles/site.css'
+import './styles/mobile-header.css'
 
 const redirect = sessionStorage.redirect
 if (redirect) {
